d6533f16e1207d9945966da25573b38da343edbd
David Aaron Suddjian <aaron@digitalrocketry.com>
2015-08-26 21:52:17 -0700

    Added AppLogic and fixed OnConfigurationChanged

--- RollifyAndroid/RollifyApplication.cs
+++ RollifyAndroid/RollifyApplication.cs
@@ -2,23 +2,27 @@
 
 using Android.App;
 using Android.Runtime;
+using Rollify.Core;
+using Android.Content.Res;
 
 namespace RollifyAndroid
 {
 	[Application]
 	public class RollifyApplication : Application
 	{
+		
+		public AppLogic Logic { get; set; }
+
 		public RollifyApplication (IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
 		{
 		}
 
 		public override void OnCreate() {
 			base.OnCreate ();
-
 		}
 
-		public override void OnConfigurationChanged() {
-			base.OnConfigurationChanged ();
+		public override void OnConfigurationChanged(Configuration newConfig) {
+			base.OnConfigurationChanged (newConfig);
 		}
 
 		public override void OnLowMemory() {
