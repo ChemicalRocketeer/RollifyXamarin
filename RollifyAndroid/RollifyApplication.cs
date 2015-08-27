c601df449274565cadc46183680b9e7c52e9ea96
David Aaron Suddjian <aaron@digitalrocketry.com>
2015-08-26 21:13:15 -0700

    Added Application Class
    This will be used to store universal app state, accessible by all
    fragments and activities.

---
+++ RollifyAndroid/RollifyApplication.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Android.App;
+using Android.Runtime;
+
+namespace RollifyAndroid
+{
+	[Application]
+	public class RollifyApplication : Application
+	{
+		public RollifyApplication (IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+		{
+		}
+
+		public override void OnCreate() {
+			base.OnCreate ();
+
+		}
+	}
+}
+
