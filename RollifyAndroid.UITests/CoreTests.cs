ed401439df81aa0b4f4566ee76a9a56f153875f8
David Aaron Suddjian <aaron@digitalrocketry.com>
2015-08-29 22:27:40 -0700

    Added a CoreTests file

---
+++ RollifyAndroid.UITests/CoreTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+
+using Rollify.Core;
+
+namespace RollifyAndroid.UITests
+{
+	[TestFixture]
+	public class CoreTests
+	{
+		[SetUp]
+		public void BeforeEachTest() {
+
+		}
+
+		[Test]
+		public void TestRollerEvaluate() {
+			
+		}
+	}
+}
+
